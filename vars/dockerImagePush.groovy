8ba17854bcfc817458ac6d6834f62567f7d70801
Nemidevops <88837045+Nemidevops@users.noreply.github.com>
2024-02-02 10:24:58 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,23 +1,8 @@
 def call(String project, String ImageTag, String hubUser){
-    withCredentials([usernamePassword(
-            credentialsId: "Docker",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
         sh """
-         docker login -u '$USER' -p '$PASS'
+         docker login -u '$DockerHubUser' -p '$pass'
         """
-    }
      sh """
-      docker image push ${hubUser}/${project}:${ImageTag}
+      docker image push ${DockerHubUser}/${project}:${ImageTag}
      """
 }
-
-
-// def call(String aws_account_id, String region, String ecr_repoName){
-    
-//     sh """
-//      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-//      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-//     """
-// }
