7f72783787c527f195e3d3287ffec7304e50c9a0
Nemidevops <88837045+Nemidevops@users.noreply.github.com>
2024-02-02 11:06:41 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -2,7 +2,7 @@ def call(String project, String ImageTag, String DockerHubUser) {
     withCredentials([usernamePassword(credentialsId: 'Docker', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
         sh """
         docker login -u '$DOCKER_USERNAME' -p '$DOCKER_PASSWORD'
-        docker image push ${DockerHubUser}/${project}:${ImageTag}
+        docker push ${DockerHubUser}/${project}:${ImageTag}
         """
     }
 }
